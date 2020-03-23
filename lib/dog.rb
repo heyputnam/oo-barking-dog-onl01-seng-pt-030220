806356885967109629b782b141d8236afe8d7e9b
Hailey P <heyputnam@gmail.com>
2020-03-23 00:23:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,12 +5,17 @@ class Dog
   end
 
   def name
-    return @name
+    puts @name
   end
 
   def bark
-    return @bark
+    puts @bark
   end
+
+  def 
 end
 
 fido = Dog.new('fido','woof!')
+
+fido.name
+fido.bark
