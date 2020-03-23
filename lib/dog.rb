357d1dcf507e60987056c78f426c961957b39ffd
Hailey P <heyputnam@gmail.com>
2020-03-23 00:05:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Dog(name)
+  @name = name
+  @bark = 'woof'
+
+  def name(name)
+    @name = name
+  end
+
+  def bark 
+    @bark = 'woof!'
+  end
+  
+
+end
+
+
+
+fido = Dog.new('fido')
+
